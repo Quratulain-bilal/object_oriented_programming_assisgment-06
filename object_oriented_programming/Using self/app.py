de8a83971f19144a72891f4c26903bff97181a9f
Quratulain-bilal <umayaimanshah@gmail.com>
2025-05-14 06:41:15 +0500

    Update app.py

--- object_oriented_programming/Using self/app.py
+++ object_oriented_programming/Using self/app.py
@@ -1,9 +1,9 @@
-# =============================================
+# =========================================
 # 1. Using self (Instance Methods)
 # Create a class Student with attributes name and marks. 
 # Use the self keyword to initialize these values via a constructor. 
 # Add a method display() that prints student details.
-# =============================================
+# =========================================
 
 class Student:
     def __init__(self, name, marks):
